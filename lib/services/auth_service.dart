9d54b3783afb61eb11bd427f07a295886c182f7e
NikitaGroshev <is911n@icloud.com>
2023-11-29 23:21:44 +0300

    Profile change: user avatar, name display, email, added the ability to change the name, email, password

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -28,4 +28,23 @@ class AuthService {
 
   // Получение текущего пользователя
   User? get currentUser => _auth.currentUser;
+
+  // Метод для повторной аутентификации пользователя
+  Future<void> reauthenticateUser(String email, String oldPassword) async {
+    User? currentUser = _auth.currentUser;
+    if (currentUser != null && email.isNotEmpty && oldPassword.isNotEmpty) {
+      AuthCredential credential = EmailAuthProvider.credential(
+        email: email,
+        password: oldPassword,
+      );
+
+      try {
+        // Повторная аутентификация
+        await currentUser.reauthenticateWithCredential(credential);
+      } on FirebaseAuthException catch (e) {
+        // Здесь можете добавить обработку различных ошибок аутентификации
+        throw FirebaseAuthException(code: e.code, message: e.message);
+      }
+    }
+  }
 }
